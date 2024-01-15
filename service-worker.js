e59eb0a12a006880e110cf6c62ac046b5f0b6a58
tropuschka <99122367+tropuschka@users.noreply.github.com>
2024-01-15 14:18:39 +0300

    Update service-worker.js
    Удалила слеши перед прекэш-манифест и индекс

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7550b6a78bf6dca8047d3da04e2eb858.js"
+  "precache-manifest.7550b6a78bf6dca8047d3da04e2eb858.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
