62bd123ced40028dc7f7146aa8cd8aa2c9c0b74a
tropuschka <99122367+tropuschka@users.noreply.github.com>
2024-01-17 15:54:27 +0300

    Add files via upload

---
+++ precache-manifest.5da09e7c71f6a12140e61f52d1cee1d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ee8c1ccbb6b98e9432238b33b0c57db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af4b3226f845dd516da8",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e40ddf2b9b671016ed71",
+    "url": "/static/js/2.ef63edac.chunk.js"
+  },
+  {
+    "revision": "af4b3226f845dd516da8",
+    "url": "/static/js/main.adb36e63.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
